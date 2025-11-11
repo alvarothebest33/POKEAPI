027dce1cb8723db45107781e85bd0a89b13e8d51
alvarothebest33 <alvarus3000@gmail.com>
2025-11-11 16:15:11 +0100

    Completa Parte 2.3 (Grupo 1): Proteger endpoints Pokémon

--- app/routers/pokemon.py
+++ app/routers/pokemon.py
@@ -1,6 +1,14 @@
-from fastapi import APIRouter, Query, Path, HTTPException, status
-from typing import Dict, Any, List
+from fastapi import APIRouter, Query, Path, HTTPException, status, Request
+from typing import Dict, Any, List, Annotated
 from app.services.pokeapi_service import PokeAPIService
+
+from fastapi import Depends
+from app.auth import get_current_user
+from app.models import User
+
+from slowapi import Limiter
+from slowapi.util import get_remote_address
+limiter = Limiter(key_func=get_remote_address)
 
 router = APIRouter(
     prefix="/pokemon",
@@ -12,7 +20,10 @@ poke_service = PokeAPIService()
 
 # ENDPOINT de listar pokemon
 @router.get("/search", response_model=Dict[str, Any])
+@limiter.limit("30/minute")
 def call_search_pokemon(
+    request: Request,
+    current_user: Annotated[User, Depends(get_current_user)],
     limit: int = Query(default=20, ge=1, le=100),
     offset: int = Query(default=0, ge=0)
 ):
@@ -30,7 +41,10 @@ def call_search_pokemon(
 
 # ENDPOINT pokemon por nombre
 @router.get("/{id_or_name}", response_model=Dict[str, Any])
+@limiter.limit("60/minute")
 def call_get_pokemon_details(
+    request: Request,
+    current_user: Annotated[User, Depends(get_current_user)],
     id_or_name: str = Path(..., description="ID o nombre del Pokémon (ej: 132 o 'ditto')")
 ):
     try:
@@ -46,7 +60,10 @@ def call_get_pokemon_details(
 
 #ENDPOINT pokemon por tipo
 @router.get("/type/{type_name}", response_model=List[Dict[str, Any]])
+@limiter.limit("60/minute")
 def call_get_pokemon_by_type(
+    request: Request,
+    current_user: Annotated[User, Depends(get_current_user)],
     type_name: str = Path(..., description="Nombre/numero del tipo (ej: 'fire', 'water', '2'. '4')")
 ):
     try:
@@ -62,7 +79,10 @@ def call_get_pokemon_by_type(
 
 # ENDPOINT especies
 @router.get("/pokeon-species/{id_or_name}", response_model=Dict[str, Any])
+@limiter.limit("60/minute")
 def call_get_pokemon_species(
+    request: Request,
+    current_user: Annotated[User, Depends(get_current_user)],
     id_or_name: str = Path(..., description="ID o nombre del Pokémon (ej: 132 o 'ditto')")
 ):
     try:
